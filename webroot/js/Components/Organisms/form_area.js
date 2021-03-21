b2e8e0365b382485431b2edc393e85fa9bb70e2e
bond-kaneko <jyunli5088@gmail.com>
2021-03-21 17:40:00 +0900

    vueのblocksをcakeに送るまで

--- webroot/js/Components/Organisms/form_area.js
+++ webroot/js/Components/Organisms/form_area.js
@@ -3,21 +3,24 @@ import { BaseInputForm } from '../Atoms/base_input_form.js'
 import { BaseSubmitButton } from '../Atoms/base_submit_button.js'
 
 export const FormArea = {
-    props: ['blocks'],
+    props: ['blocks', 'csrfToken'],
     template:`
         <div class="form-area column-40 content">
-            <form method="post" accept-charset="utf-8" action="/portfolios/add">
+            <form method="post" accept-charset="utf-8" action="/instructor/portfolios/add">
                 <fieldset>
+                    <input type="hidden" name="_csrfToken" :value="csrfToken" >
                     <legend>ポートフォリオ作成</legend>
-                    <div class="input text required error">
+                        <div class="input text required error">
                         <label for="title">Title</label>
                         <base-input-form :name="title"></base-input-form>
 
                         <legend>コンテント</legend>
+                        <input type="hidden" name="content" :value="JSON.stringify(blocks)">
                         <content-area :blocks="blocks"></content-area>
                     </div>
                 </fieldset>
                 <base_submit_button text='作成'></base_submit_button>
+                
             </form>
         </div>
     `,
